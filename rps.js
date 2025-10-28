86052e1e2376553b2b00dc98777d6f77a91a31fc
Lily <40526330+Umgak@users.noreply.github.com>
2025-10-28 00:44:38 -0500

    Prototype of playRound()
    Probably needs a rework at some point, but seems to work fine for now.

--- rps.js
+++ rps.js
@@ -9,21 +9,37 @@ function playGame()
     { name: "scissors", beats: ["paper"]},
     // secret option
     { name: "trans rights", beats: ["rock", "paper", "scissors"]}
-  ]
+  ];
+
+  const RoundState = Object.freeze({
+    /* pseudo-enum for roundstate */
+    /* syntax: NAME: [player_score, computer_score] */
+    NO_VICTOR: [0, 0],
+    PLAYER_VICTOR: [1, 0],
+    COMPUTER_VICTOR: [0, 1]
+  });
 
   function getComputerChoice()
   {
-    return Choice[(Math.floor(Math.random() * (Choice.length - 1)))] // -1 so that computer cannot pick trans rights
+    return Choice[(Math.floor(Math.random() * (Choice.length - 1)))]; // -1 so that computer cannot pick trans rights
   }
 
   function getPlayerChoice()
   {
     let playerChoice = prompt("Enter rock, paper, or scissors.").toLowerCase();
-    return Choice.find(obj => obj.name === playerChoice)
+    return Choice.find(obj => obj.name === playerChoice);
   }
 
-  function playRound() {
-
+  function playRound(playerChoice, computerChoice) {
+    if (playerChoice.beats.includes(computerChoice.name))
+    {
+      return RoundState.PLAYER_VICTOR;
+    }
+    else if (computerChoice.beats.includes(playerChoice.name))
+    {
+      return RoundState.COMPUTER_VICTOR;
+    }
+    return RoundState.NO_VICTOR;
   }
 
 }
