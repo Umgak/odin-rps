1cccc834ec0460200a7858897345348038d24d83
Lily <40526330+Umgak@users.noreply.github.com>
2025-10-28 00:23:45 -0500

    feat: overengineered selector
    very, very overengineered. This is still x50 level assignments and I'm
    treating it like this is going to be graded. Bleh.

--- rps.js
+++ rps.js
@@ -1,6 +1,16 @@
 "use strict;"
 function playGame()
 {
+  const Choice = [
+    /* lets me add extra options easily - in case this becomes rock-paper-scissors-lizard-spock tomorrow
+       I know that you don't expect me to know array or object yet. I do. Sorrgy :3 */
+    { name: "rock", beats: ["scissors"]},
+    { name: "paper", beats: ["rock"]},
+    { name: "scissors", beats: ["paper"]},
+    // secret option
+    { name: "trans rights", beats: ["rock", "paper", "scissors"]}
+  ]
+
   function getComputerChoice()
   {
 
