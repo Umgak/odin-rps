f9500d9665e5b8628ac3fb48b00a4810cafca961
Lily <40526330+Umgak@users.noreply.github.com>
2025-10-29 11:47:30 -0500

    cleanup: better handling of trans rights EE
    Forgot to commit for a bit. Sorry.

--- rps.js
+++ rps.js
@@ -1,6 +1,9 @@
 "use strict;"
 function playGame()
 {
+  const MAX_ROUNDS = 5;
+  let humanScore = 0, computerScore = 0;
+  let usedTransRights = false;
   const Choice = [
     /* lets me add extra options easily - in case this becomes rock-paper-scissors-lizard-spock tomorrow
        I know that you don't expect me to know array or object yet. I do. Sorrgy :3 */
@@ -10,6 +13,16 @@ function playGame()
     // secret option
     { name: "trans rights", beats: ["rock", "paper", "scissors"]}
   ];
+
+  function disableTransRightsEE()
+  /* only let you use the trans rights easter egg once per game */
+  {
+    for (let i = 0; i < (Choice.length - 2); ++i) {
+      Choice[i].beats.push("trans rights");
+    }
+    Choice[Choice.length - 1].beats.length = 0;
+    return;
+  }
 
   const RoundState = Object.freeze({
     /* pseudo-enum for roundstate */
@@ -40,14 +53,18 @@ function playGame()
         query = `Unable to understand player choice: ${playerInput}. Please try again.\n\n${query}`;
         runOnce = true; // don't modify the text multiple times
       }
-
     } while (playerChoice === undefined)
     return playerChoice;
   }
 
-  function playRound(playerChoice, computerChoice) {
+  function determineVictor(playerChoice, computerChoice)
+    /* the *only* purpose of this function is to compute a victor. That's it. */
+    {
     if (playerChoice.beats.includes(computerChoice.name))
     {
+      if (playerChoice.name === "trans rights") {
+        disableTransRightsEE()
+      }
       return RoundState.PLAYER_VICTOR;
     }
     else if (computerChoice.beats.includes(playerChoice.name))
@@ -57,4 +74,14 @@ function playGame()
     return RoundState.NO_VICTOR;
   }
 
+  function playRound()
+  /* according to the instructions, I'm supposed to get the player choices outside of this function.
+     no. */
+  {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    let scoreDelta = determineVictor(playerChoice, computerChoice);
+    humanScore += scoreDelta[0];    // avoids using big if-else blocks for playRound (they're in determineVictor instead)
+    computerScore += scoreDelta[1]; // this is actually faster than using a loop, w/e
+  }
 }
